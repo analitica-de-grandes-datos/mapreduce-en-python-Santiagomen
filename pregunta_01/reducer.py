8626233e12740a91b00fcc9284f6ca715f10a0e5
Santiagomen <95106084+Santiagomen@users.noreply.github.com>
2023-05-20 14:35:10 -0500

    Update reducer.py

--- pregunta_01/reducer.py
+++ pregunta_01/reducer.py
@@ -1,3 +1,18 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys 
+dic = {}
+for linea in lineas:
+    linea = linea.strip()
+    valores = linea.split(',')
+    r = valores[2]
+    
+    if r in dic:
+       dic[valores[2]] += 1
+    else:
+       dic[valores[2]] = 1
+dic_ordenado = dict(sorted(dic.items(), key=lambda x: x[0]))
+
+for clave, valor in dic_ordenado.items():
+    print(clave, valor)
