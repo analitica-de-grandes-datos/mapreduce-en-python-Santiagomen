a33f8daa8da102d27188ff0d6c3bc6bb3d8d0978
Santiagomen <95106084+Santiagomen@users.noreply.github.com>
2023-05-22 17:11:08 -0500

    Update reducer.py

--- pregunta_02/reducer.py
+++ pregunta_02/reducer.py
@@ -1,3 +1,20 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys
+
+dict = {}
+for row in sys.stdin:
+  r = row.strip()
+  r = r.split(";")
+  if r[0] in dict.keys():
+    if int(r[1]) > dict[r[0]]:
+      dict[r[0]] = int(r[1])
+    else:
+      continue
+  else:
+    dict[r[0]] = int(r[1])
+for i in dict.keys():
+   letra = i
+   linea =  letra + "\t" + str(dict[i]) + "\n"
+   sys.stdout.write(linea)  
