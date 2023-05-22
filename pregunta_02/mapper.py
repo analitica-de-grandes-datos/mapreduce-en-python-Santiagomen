99248964aa2cb105db867e14a15ad8c77fdc907f
Santiagomen <95106084+Santiagomen@users.noreply.github.com>
2023-05-22 14:25:58 -0500

    Update mapper.py

--- pregunta_02/mapper.py
+++ pregunta_02/mapper.py
@@ -1,3 +1,10 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys
+
+for linea in sys.stdin:
+    linea = linea.strip()
+    valores = linea.split(',')
+    amount = valores[4]
+    print(amount)
