8615d9e90c9684634b07266f6309c03afeb2e7d1
Santiagomen <95106084+Santiagomen@users.noreply.github.com>
2023-05-25 21:04:16 -0500

    Update reducer.py

--- pregunta_04/reducer.py
+++ pregunta_04/reducer.py
@@ -1,3 +1,17 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+if __name__ == "__main__":
+    dic = {}
+    for linea in sys.stdin:
+        columna = line.split("\n")
+        letra = columna[0]
+        if letra in dic:
+            dic[letra]+= 1
+            
+        else:
+            dic[letra]= 1
+            
+    dic_sort= sorted(dic.items())
+    for atributo,valor in dic_sort:
+        sys.stdout.write(f"{atributo},{valor}\n")
