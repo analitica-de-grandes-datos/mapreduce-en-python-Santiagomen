53fa8b5e34eaf0d2c56ea437d056d434779106fa
Santiagomen <95106084+Santiagomen@users.noreply.github.com>
2023-05-25 21:19:08 -0500

    Update reducer.py

--- pregunta_05/reducer.py
+++ pregunta_05/reducer.py
@@ -1,3 +1,19 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys
+
+if __name__ == "__main__":
+    meses = {}
+    for linea in sys.stdin:
+        columns = linea.split("\n")
+        mes = columns[0]
+        if mes in meses:
+            meses[mes]+= 1
+            
+        else:
+            meses[mes]= 1
+            
+    resume_sort= sorted(meses.items())
+    for atributo,valor in total_sort:
+        sys.stdout.write(f"{atributo}\t{valor}\n")
