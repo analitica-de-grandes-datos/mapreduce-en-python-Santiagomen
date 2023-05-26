0438bd87fb20911b5dba8a62172f43cb8979d431
Santiagomen <95106084+Santiagomen@users.noreply.github.com>
2023-05-25 21:16:46 -0500

    Update mapper.py

--- pregunta_05/mapper.py
+++ pregunta_05/mapper.py
@@ -1,3 +1,11 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys
+
+if __name__ == "__main__":
+    for linea in sys.stdin:
+        columna = linea.split(" ")
+        fecha = columna[1].split("-")
+        mes = fecha[1]
+        sys.stdout.write(f"{mes}\n"
