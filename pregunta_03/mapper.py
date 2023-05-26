a8e8b0e591df442e3ffa93c3b98d53a983a40c46
Santiagomen <95106084+Santiagomen@users.noreply.github.com>
2023-05-25 19:03:45 -0500

    Update mapper.py

--- pregunta_03/mapper.py
+++ pregunta_03/mapper.py
@@ -1,3 +1,8 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys 
+
+for line in sys.stdin:
+    letra, val = line.strip().split(',')
+    sys.stdout.write("{},{}\n".format(letra,val))
