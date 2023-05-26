ce2572a34fe58341ee5f66979c75f1af363f71d1
Santiagomen <95106084+Santiagomen@users.noreply.github.com>
2023-05-25 19:37:49 -0500

    Update reducer.py

--- pregunta_03/reducer.py
+++ pregunta_03/reducer.py
@@ -1,3 +1,16 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys
+
+  dict = {}
+  for linea in sys.stdin:
+      columna = linea.split("\t")
+     
+      letra = columna[0] 
+      val = int(columna[1])
+      dict[letra] = val
+
+  dict_sort = sorted(dict.items(), key=lambda x: x[1])
+  for atributo,valor in dict_sort:
+      sys.stdout.write(f"{atributo},{val}\n")
