d5536511c13b565b32b264eb82a69321d11be974
Santiagomen <95106084+Santiagomen@users.noreply.github.com>
2023-05-26 23:12:58 -0500

    Update reducer.py

--- pregunta_07/reducer.py
+++ pregunta_07/reducer.py
@@ -1,3 +1,21 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys
+
+if __name__ == "__main__":
+    lista = []
+    for linea in sys.stdin:
+        columna = linea.split(",")
+        
+        if len(columna)==3:
+            letra = columna[0]
+            #date = columna[1].split('-')
+            fecha = columna[1]
+            val = int(columna[2])
+            lista.append((letra, fecha, val))
+
+    lista_sorted = sorted(lista, key = lambda x: (x[0], x[2]))
+
+    for k in lista_sorted:
+        sys.stdout.write(f"{k[0]}   {k[1]}   {k[2]}\n")
