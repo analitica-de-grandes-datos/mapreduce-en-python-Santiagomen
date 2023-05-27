e1ad62ee694299eb90c6aa6bc58727f0c24735f3
Santiagomen <95106084+Santiagomen@users.noreply.github.com>
2023-05-26 20:31:36 -0500

    Update reducer.py

--- pregunta_06/reducer.py
+++ pregunta_06/reducer.py
@@ -1,3 +1,27 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys
+
+if __name__ == "__main__":
+    dic = {}
+    
+    for linea in sys.stdin:
+        columna = linea.split(",")
+        if len(columna) == 2:
+            letra = columna[0]
+            val = float(columna[1])
+            
+            if letra in dic:
+                dic[letra] = {'max': max(dic[letter]['max'], val),'min': min(dic[letter]['min'], val)}
+                #print(letra, val)
+            else:
+                dic[letter] = {'max': val, 'min': val}
+                
+    #maximo = max(lista_valores)
+    #minimo = min(lista_valores)
+    #print(dic[j], maximo, minimo)
+    
+    dic = sorted(dic.items(), key=lambda x: x[0])
+    for atributo, valor in dic:
+        sys.stdout.write(f"{atributo}\t{valor['max']}\t{valor['min']}\n")
