f88c3e5f47349c7a308d75d01e2c1247c472b3a1
Santiagomen <95106084+Santiagomen@users.noreply.github.com>
2023-05-26 20:38:40 -0500

    Update mapper.py

--- pregunta_07/mapper.py
+++ pregunta_07/mapper.py
@@ -1,3 +1,13 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys
+
+if __name__ == "__main__":
+    for linea in sys.stdin:
+        columna = linea.split("   ")
+        letra = columna[0]
+        fecha = columna[1]
+        val = columna[2]
+       
+        sys.stdout.write(f"{letra},{fecha},{val}\n")
