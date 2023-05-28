88c8573c3d529e538f8dfad94288437abddb8060
Santiagomen <95106084+Santiagomen@users.noreply.github.com>
2023-05-28 18:36:42 -0500

    Update reducer.py

--- pregunta_10/reducer.py
+++ pregunta_10/reducer.py
@@ -1,3 +1,26 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys
+
+if __name__ == "__main__":
+    dic = {}
+    
+    for linea in sys.stdin:
+        columna = linea.split("\t")
+        if len(columna) == 2:
+            val = columna[0]
+            colletras = columna[1].strip()
+            
+            for letra in colletras:
+                if letra in dic:
+                    dic[letra].append(val)
+                else:
+                    dic[letra] = [val]
+
+    dic_sorted = sorted(dic.items(), key=lambda x: x[0])
+    
+    for letra, values in dic_sorted:
+        values_sorted = sorted(values, key=lambda x: int(x))
+        values_str = ",".join(values_sorted)
+        sys.stdout.write(f"{letra}\t{values_str}\n")
