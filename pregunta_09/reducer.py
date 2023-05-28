f19f9e80b49bb75a3db7439e6caefa5bb9fe0ac7
Santiagomen <95106084+Santiagomen@users.noreply.github.com>
2023-05-28 18:31:23 -0500

    Update reducer.py

--- pregunta_09/reducer.py
+++ pregunta_09/reducer.py
@@ -1,3 +1,31 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys
+
+if __name__ == "__main__":
+    dic = {}
+    contador = 0
+    
+    for linea in sys.stdin:
+        columna = linea.split(",")
+        
+        if len(columna) >= 3:
+            letra = columna[0]
+            fecha = columna[1]
+            val = int(columna[2])
+            
+            if val in dic:
+                if dic[val] > (letra, fecha):
+                    dic[val] = (letra, fecha)
+            else:
+                dic[val] = (letra, fecha)
+    
+    dic_sorted = sorted(dic.items())
+    
+    for val, (letra, fecha) in dic_sorted:
+        sys.stdout.write(f"{letra}   {fecha}   {val}\n")
+        contador += 1
+        
+        if contador == 6:
+            break
